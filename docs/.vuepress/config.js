0e3b6c3658fe18ab39c05040a0f43197f00f89b3
womeng <donglongqin@qq.com>
2026-01-07 15:00:27 +0800

    侧边栏去掉分组

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,36 +8,29 @@ export default defineUserConfig({
   title: '我梦笔记',
   description: '欢迎来到我梦的笔记文档空间',
 
-  theme: defaultTheme({
-    logo: 'https://vuejs.press/images/hero.png',
+theme: defaultTheme({
+  logo: 'https://vuejs.press/images/hero.png',
 
-    navbar: ['/', '/guide/MySql'],
+  navbar: ['/', '/guide/MySql'],
 
-    sidebar: {
-      '/guide/': [
-        {
-          text: '指南',
-          collapsible: false,
-          children: [
-            '/guide/MySql.md',
-            '/guide/Java.md',
-            '/guide/Jvm.md',
-            '/guide/Spring.md',
-            '/guide/SpringBoot.md',
-            '/guide/Redis.md',
-            '/guide/Redis分布式锁.md',
-            '/guide/数据结构.md',
-            '/guide/设计模式.md',
-            '/guide/Linux.md',
-            '/guide/Nginx.md',
-            '/guide/vue.md',
-            '/guide/OpenWrt.md',
-          ],
-        },
-      ],
-    },
-
-  }),
+  sidebar: {
+    '/guide/': [
+      '/guide/MySql.md',
+      '/guide/Java.md',
+      '/guide/Jvm.md',
+      '/guide/Spring.md',
+      '/guide/SpringBoot.md',
+      '/guide/Redis.md',
+      '/guide/Redis分布式锁.md',
+      '/guide/数据结构.md',
+      '/guide/设计模式.md',
+      '/guide/Linux.md',
+      '/guide/Nginx.md',
+      '/guide/vue.md',
+      '/guide/OpenWrt.md',
+    ],
+  },
+}),
 
   bundler: webpackBundler(),
 })
